8ed573c4c5289656817e9da45721014bca4c0686
RayRacine <ray.racine@gmail.com>
2011-08-27 19:22:57 -0400

    AWS authentication procedures are not typed.

--- src/racket/aws/auth.rkt
+++ src/racket/aws/auth.rkt
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Knozama's Amazon API Library
-;; Copyright (C) 2007,2008,2009,2010  Raymond Paul Racine
+;; Copyright (C) 2007-2011  Raymond Paul Racine
 ;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -16,38 +16,40 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-#lang racket/base
+#lang typed/racket/base
 
-(provide md5-file 
-	 aws-s3-auth-str
-	 aws-s3-auth-mac
-	 aws-s3-auth-mac-encode)
+(provide
+ aws-auth-str
+ aws-auth-mac
+ aws-auth-mac-encode)
   
-(require net/base64
-	 net/uri-codec
-	 file/md5
-	 (only-in srfi/13
-		  string-trim-both)
-	 web-server/stuffers/hmac-sha1
-	 (only-in (planet knozama/common:1/type/list)
-		  weave))
+(require/typed srfi/13
+	       (string-trim-both (String -> String)))
 
-
-(define (md5-file fname)
-  (let ((inf (open-input-file fname 'binary)))
-    (let ((hash (md5 inf)))
-      (close-input-port inf)
-      hash)))    
-
-(define (aws-s3-auth-str verb md5 mime expiration amz-headers resource)
+(require
+ (only-in (planet knozama/common:1/text/util)
+	  weave-string-separator)
+ (only-in (planet knozama/webkit:1/crypto/base64)
+	  base64-encode)
+ (only-in (planet knozama/webkit:1/crypto/hmac)
+	  hmac-sha1)
+ (only-in (planet knozama/webkit:1/web/uri)
+	  url-encode-string))
+ 
+(: aws-auth-str (String String String String (Listof String) String -> String))
+(define (aws-auth-str verb md5 mime expiration amz-headers resource)
   (let ((sep "\n"))
     (if (null? amz-headers)
-       (weave (list verb md5 mime expiration resource) sep)
-       (weave (list verb md5 mime expiration (weave amz-headers sep) resource) sep))))
+       (weave-string-separator sep (list verb md5 mime expiration resource))
+       (weave-string-separator sep (list verb md5 mime expiration 
+				     (weave-string-separator sep amz-headers) resource)))))
 
-(define (aws-s3-auth-mac key str)
-  (string-trim-both (bytes->string/utf-8 (base64-encode (HMAC-SHA1 (string->bytes/utf-8 key) (string->bytes/utf-8 str))))))
+(: aws-auth-mac (String String -> String))
+(define (aws-auth-mac key str)
+  (string-trim-both (base64-encode (hmac-sha1 (string->bytes/utf-8 key) 
+					      (string->bytes/utf-8 str)))))
 
-(define (aws-s3-auth-mac-encode key str)
-  (uri-encode (aws-s3-auth-mac key str)))
+(: aws-auth-mac-encode (String String -> String))
+(define (aws-auth-mac-encode key str)
+  (url-encode-string (aws-auth-mac key str) #f))
 
