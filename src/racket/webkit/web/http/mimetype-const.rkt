22f99932abb5244a2a32b6aac628f5fe4759354e
RayRacine <ray.racine@gmail.com>
2011-08-27 19:15:36 -0400

    Start of place for standard mimetypes and utility functions.

---
+++ src/racket/webkit/web/http/mimetype-const.rkt
@@ -0,0 +1,8 @@
+#lang typed/racket/base
+
+(provide all-defined-out)
+
+;; Standard Header Values
+
+(define JSON "application/json")
+(define X-WWW-FORM-URLENCODED "application/x-www-form-urlencoded")
