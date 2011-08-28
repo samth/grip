ddc879217f689388a6f5e5c9cf5cde53f32ac84a
RayRacine <ray.racine@gmail.com>
2011-08-27 21:21:11 -0400

    S3 examples.

---
+++ src/racket/examples/aws/s3/s3-examples.rkt
@@ -0,0 +1,17 @@
+#lang typed/racket/base
+
+(require 
+ (only-in (planet knozama/aws:1/credential)
+	  current-aws-credential
+	  init-aws-credential)
+ (only-in (planet knozama/aws:1/s3/s3)
+	  list-buckets))
+
+(: credential-path Path)
+(define credential-path (string->path "/home/ray/awsaccount.txt"))
+
+(init-aws-credential credential-path)
+
+(: my-buckets (-> Void))
+(define (my-buckets)
+  (list-buckets ""))
