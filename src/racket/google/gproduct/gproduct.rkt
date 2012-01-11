61a99c6005aa15c056bc11309759861f5e7f345f
RayRacine <ray.racine@gmail.com>
2012-01-11 08:47:59 -0500

    Google product search API call.

--- src/racket/google/gproduct/gproduct.rkt
+++ src/racket/google/gproduct/gproduct.rkt
@@ -4,6 +4,8 @@
 
 (require
  racket/pretty
+ (only-in (planet knozama/xml:1/sxml)
+	  sxpath)
  (only-in (planet knozama/webkit:1/web/http/header)
           make-header-string)
  (only-in (planet knozama/webkit:1/web/uri)
@@ -13,7 +15,7 @@
  (only-in (planet knozama/webkit:1/web/http/http11)
 	  HTTPConnection-in http-successful? http-close-connection http-invoke)
  (only-in (planet knozama/xml:1/sxml)
-	  Sxml SXPath html->sxml)
+	  Sxml SXPath xml->sxml extract-text extract-integer)
  (only-in "access.rkt"
 	  load-key)
  (only-in "config.rkt"
@@ -25,7 +27,10 @@
 (struct: SearchResult ([company : String]
 		       [account : Integer]
 		       [title : String]
-		       [link : String]))
+		       [gtin  : String]
+		       [link : String]) #:transparent)
+
+;; HTTP Stuff
 
 (: search-request-headers (Listof String))
 (define search-request-headers  (list 
@@ -42,30 +47,62 @@
 (define (query-q-param query)
   (cons "q" (encode-param-string query #t)))
 
+(: access-key-param (Pairof String String))
+(define access-key-param (cons "key" key)) 
+
 (: make-query-uri ((Listof (Pair String String)) -> Uri))
 (define (make-query-uri queries)
   (make-uri "https" #f gproduct-host 443 gproduct-path 
 	    (params->query (append queries std-query-params)) ""))
 
+;; Response parsing stuff
+
+(: sx-prods SXPath)
+(define sx-prods (sxpath "/feed/entry/product" '()))
+
+(: sx-name SXPath)
+(define sx-name (sxpath "/author/name/text()" '()))
+
+(: sx-account SXPath)
+(define sx-account (sxpath "/author/accountid/text()" '()))
+
+(: sx-title SXPath)
+(define sx-title (sxpath "/title/text()" '()))
+
+(: sx-link SXPath)
+(define sx-link (sxpath "/link/text()" '()))
+
+(: sx-gtin SXPath)
+(define sx-gtin (sxpath "/gtin/text()" '()))
+
+(: parse-search-result (Sxml -> (Option SearchResult)))
+(define (parse-search-result page)
+  (let ((prods (sx-prods page)))
+    (let ((gtin (extract-text (sx-gtin prods)))
+	(title (extract-text (sx-title prods)))
+	(name (extract-text (sx-name prods)))
+	(account (extract-integer (sx-account prods)))
+	(link (extract-text (sx-link prods))))
+      (pretty-print (sx-link prods))
+      (pretty-print name)
+      (pretty-print gtin)
+      (pretty-print account)
+      (if (and account gtin)
+	 (SearchResult name account title gtin link)
+	 #f))))
+
 (: product-search (String -> (Option SearchResult)))
 (define (product-search query)
-  (let ((url (make-query-uri (list (cons "q" (encode-param-string query #f))))))
-    (pretty-print url)
-    #f))
-
-;; (: test-uri Uri)
-;; (define test-uri
-;;   (assert (parse-uri (string-append "https://www.googleapis.com/shopping/search/v1/public/products?country=US&brand=smead&q=smead+153L&fields=entry/s:product(s:author,s:gtin,s:title,s:link)&alt=atom&key=" key))))
-
-;; (: test (-> (Option Sxml)))
-;; (define (test)
-;;   (pretty-print test-uri)
-;;   (let ((conn (http-invoke 'GET test-uri search-request-headers #f)))
-;;     (pretty-print conn)
-;;     (if (http-successful? conn)
-;;        (let ((page (html->sxml (HTTPConnection-in conn))))
-;; 	 (http-close-connection conn)
-;; 	 page)
-;;        (begin
-;; 	 (http-close-connection conn)
-;; 	 #f))))
+  (let ((url (make-query-uri (list (query-q-param query) access-key-param))))
+    (with-handlers ([exn:fail? 
+		     (lambda (ex) #f)])
+      (let ((conn (http-invoke 'GET url search-request-headers #f)))
+	(pretty-print conn)
+	(if (http-successful? conn)       
+	   (let ((page (xml->sxml (HTTPConnection-in conn) '("http://www.google.com/shopping/api/schemas/2010"))))
+	     (pretty-print page)
+	     (http-close-connection conn)
+	     (parse-search-result page))
+	   (begin
+	     (http-close-connection conn)
+	     #f))))))
