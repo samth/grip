1901bf2c2599338159b0d012808a009a368de993
RayRacine <ray.racine@gmail.com>
2012-01-29 23:15:48 -0500

    Common struct: types in a single source file.

--- src/racket/aws/dynamodb/types.rkt
+++ src/racket/aws/dynamodb/types.rkt
@@ -1,6 +1,21 @@
 #lang typed/racket/base
 
 (provide
- DDBError DDBError? DDBError-code)
+ DDBError DDBError? DDBError-code
+ ddbtype-code ddbtype-symbol DDBType)
 
 (struct: DDBError ([code : Symbol]))
+
+(define-type DDBType (U 'String 'Number))
+
+(: ddbtype-code (DDBType -> String))
+(define (ddbtype-code type)
+  (case type
+    ((String) "S")
+    ((Number) "N")))
+
+(: ddbtype-symbol (DDBType -> Symbol))
+(define (ddbtype-symbol type)
+  (case type
+    ((String) 'S)
+    ((Number) 'N)))
