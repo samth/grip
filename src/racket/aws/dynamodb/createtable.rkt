83db7cc5932569c70e22da6280b94cffd78469f8
RayRacine <ray.racine@gmail.com>
2012-01-29 23:15:09 -0500

    Createtable command.

--- src/racket/aws/dynamodb/createtable.rkt
+++ src/racket/aws/dynamodb/createtable.rkt
@@ -1,21 +1,71 @@
 #lang typed/racket/base
 
+(provide create-table
+	 Key Key? Key-name Key-type
+	 Throughput Throughput? Throughput-read Throughput-write)
+
 (require 
+ racket/pretty
+ (only-in (planet knozama/webkit:1/formats/tjson)
+	  Json JsObject json->string)
  (only-in "types.rkt"
-	  DDBError DDBError? DDBError-code))
+	  DDBType ddbtype-code)
+ (only-in "action.rkt"
+	  CREATE-TABLE)
+ (only-in "types.rkt"
+	  DDBError DDBError? DDBError-code)
+ (only-in "invoke.rkt"
+	  dynamodb))
 
 (struct: Throughput ([read : Natural] 
 		     [write : Natural]) #:transparent)
 
 (struct: Key ([name : String]
-	      [type : String]) #:transparent)
+	      [type : DDBType]) #:transparent)
 
-(struct: Schema ([name : String]
-		 [hash-key : Key]
-		 [range-key : (Option Key)]
-		 [throughput : Throughput]) #:transparent)
+(struct: CreateTableResp ())
 
-(: create-table (String Schema Throughput -> (U DDBError CreateTableResp)))
-(define (create-table name schema throughput)
-  )
+(: create-request (String Key (Option Key) Throughput -> String))
+(define (create-request name hash-key range-key throughput)
 
+  (: keys-json (Key (Option Key) -> JsObject))
+  (define (keys-json hash-key range-key)
+    (: key-json (Key -> JsObject))
+    (define (key-json key)
+      (make-hasheq `((AttributeName . ,(Key-name key))
+		     (AttributeType . ,(ddbtype-code (Key-type key))))))
+
+    (let: ((keys : JsObject (make-hasheq)))
+      (hash-set! keys 'HashKeyElement (key-json hash-key))
+      (when range-key
+	(hash-set! keys 'RangeKeyElement (key-json hash-key)))
+      keys))
+
+  (: throughput-json (Throughput -> JsObject))
+  (define (throughput-json throughput)
+    (make-hasheq `((ReadCapacityUnits . ,(Throughput-read throughput))
+		   (WriteCapacityUnits . ,(Throughput-write throughput)))))
+
+  (json->string (make-hasheq `((TableName . ,name)
+			       (KeySchema . ,(keys-json hash-key range-key))
+			       (ProvisionedThroughput . ,(throughput-json throughput))))))
+
+(: create-table (String Key (Option Key) Throughput -> (U DDBError CreateTableResp)))
+(define (create-table name hash-key range-key throughput) 
+  (pretty-print (dynamodb CREATE-TABLE (create-request name hash-key range-key throughput)))
+  (DDBError 'Test))
+
+
+(define (test)  
+  (create-table "ray" (Key "sku" 'String) #f (Throughput 3 5)))
+
+;; POST / HTTP/1.1 
+;; x-amz-target: DynamoDB_20111205.CreateTable 
+;; content-type: application/x-amz-json-1.0 
+
+;; {"TableName":"Table1",
+;;     "KeySchema":
+;;         {"HashKeyElement":{"AttributeName":"AttributeName1","AttributeType":"S"},
+;;         "RangeKeyElement":{"AttributeName":"AttributeName2","AttributeType":"N"}},
+;;     "ProvisionedThroughput":{"ReadCapacityUnits":5,"WriteCapacityUnits":10}
+;; }
