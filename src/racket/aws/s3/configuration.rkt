a4520b1d5c6e7c985929932e89bc220e8592ad1f
RayRacine <ray.racine@gmail.com>
2012-02-21 22:20:56 -0500

    WIP Resurrecting and refactoring S3 code.

--- src/racket/aws/s3/configuration.rkt
+++ src/racket/aws/s3/configuration.rkt
@@ -1,7 +1,10 @@
 #lang typed/racket/base
 
 (provide
- s3-namespace)
+ nss s3-namespace)
 
 (: s3-namespace String)
 (define s3-namespace "http://s3.amazonaws.com/doc/2006-03-01/")
+
+(: nss (List (Pair Symbol String)))
+(define nss `((s3 . ,s3-namespace)))
---
+++ src/racket/aws/s3/invoke.rkt
@@ -0,0 +1,116 @@
+#lang typed/racket/base
+
+(provide 
+ S3Response S3Response? S3Response-http S3Response-sxml
+ make-base-uri
+ s3-invoke)
+
+(require
+ racket/pretty
+ (only-in (planet knozama/webkit:1/web/uri)
+	  make-uri Uri Uri-path)
+ (only-in (planet knozama/common:1/type/date)
+	  current-date-string-rfc-2822)
+ (only-in (planet knozama/webkit:1/web/http/heading)
+	  DATE HOST)
+ (only-in (planet knozama/webkit:1/web/http/header)
+	  Header Headers
+	  header->string
+	  make-header
+	  date-header
+	  content-length
+	  content-type
+	  content-md5)
+ (only-in (planet knozama/webkit:1/web/uri/url/param)
+	  Params
+	  params->query)
+ (only-in (planet knozama/webkit:1/web/http/http11)
+	  Action 
+	  http-action->string http-status-code http-has-content?
+	  ResponseHeader-result Result
+	  HTTPConnection-in HTTPConnection-header
+	  http-invoke http-close-connection make-client-error-response)
+ (only-in (planet knozama/xml:1/sxml)
+	  Sxml SXPath 
+	  sxpath xml->sxml select-single-node-text)
+ (only-in "../credential.rkt"
+	  current-aws-credential
+	  AwsCredential
+	  BaseCredential-access-key
+	  BaseCredential-secret-key)
+ (only-in "../auth.rkt"
+	  aws-auth-str
+	  aws-auth-mac)
+ (only-in "../configuration.rkt"
+	  s3-host)
+ (only-in "configuration.rkt"
+	  s3-namespace nss))
+
+(struct: S3Response ([http : Result]
+		     [sxml : Sxml]) #:transparent)
+
+(: empty-response (List Symbol))
+(define empty-response '(*TOP*))
+
+(: make-base-uri ((Option String) String (Option Params) -> (Option Uri)))
+(define (make-base-uri bucket path params)
+
+  (: make-bucket-host (-> String))
+  (define (make-bucket-host)
+    (if bucket
+	(string-append bucket "." s3-host)
+	s3-host))
+
+  (let ((query (if params 
+		   (params->query params)
+		   #f)))
+    (make-uri "http" #f (make-bucket-host) 80 path query #f)))
+
+(: authorization-header (AwsCredential String -> Header))
+(define (authorization-header credential auth-str)
+  (make-header "Authorization" 
+	       (string-append  "AWS " 
+			       (BaseCredential-access-key credential) 
+			       ":" 
+			       (aws-auth-mac (BaseCredential-secret-key credential)
+					     auth-str))))
+
+(: s3-invoke (Action (Option String) String (Option Params) Headers -> S3Response))
+(define (s3-invoke action bucket path query-params headers)
+  (let ((url (make-base-uri bucket path query-params)))
+    (if url
+	(let* ((datetime (current-date-string-rfc-2822))
+	       (canonical-resource (if bucket
+				       (string-append "/" bucket (Uri-path url))
+				       (Uri-path url)))
+	       (core-headers (map header->string 
+				  (list (make-header DATE datetime)
+					(authorization-header (current-aws-credential)
+							      (aws-auth-str (http-action->string action)
+									    "" "" 
+									    datetime '()
+									    canonical-resource))))))
+	  (let ((connection (http-invoke action 
+					 url 
+					 (append core-headers (map header->string headers))
+					 #f)))
+	    (with-handlers [(exn:fail? (lambda (ex)
+					 ((error-display-handler) "ERROR in S3 invocation." ex)
+					 (displayln ex) 
+					 (http-close-connection connection)
+					 (S3Response (Result "HTTP1.1" 440
+							     (string-append "Bad Request - "
+									    (exn-message ex)))
+						     empty-response)))]
+	      
+	      (if (http-has-content? connection)
+		  (let ((results (xml->sxml (HTTPConnection-in connection) '())))
+		    (http-close-connection connection)
+		    (S3Response (ResponseHeader-result (HTTPConnection-header connection))
+				results))
+		  (S3Response (ResponseHeader-result (HTTPConnection-header connection))
+			      empty-response)))))
+	(S3Response (Result "HTTP/1.1" 400 
+			    (string-append "Bad Request - Malformed URL"))
+		    empty-response))))
+
