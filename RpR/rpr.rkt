6f7c3e0219757cc9395acd6f7797471ccba0e318
Ray Racine <ray.racine@gmail.com>
2012-12-22 14:50:35 -0500

    Commented out small bits of plot code until NToronto's TR of Plot lands.

--- RpR/rpr.rkt
+++ RpR/rpr.rkt
@@ -1,27 +1,27 @@
 #lang typed/racket/base
 
 (provide
- (all-from-out typed/plot/main)
+ ;(all-from-out typed/plot/main)
  (all-from-out "load/load.rkt")
  (all-from-out "load/tab-delimited.rkt")
  (all-from-out "load/schema.rkt")
  (all-from-out "frame/frame.rkt")
  (all-from-out "stats/tabulate.rkt")
  (all-from-out "stats/statistics.rkt")
- (all-from-out "plot/plot.rkt")
+ ;(all-from-out "plot/plot.rkt")
  (all-from-out "frame/numeric-series.rkt")
  (all-from-out "frame/categorical-series.rkt")
  (all-from-out "frame/gen-nseries.rkt"))
                
 (require 
- typed/plot/main
+ ;typed/plot/main
  (only-in "load/load.rkt"          
           load-tab-delimited-file)
  "frame/frame.rkt"
  "frame/numeric-series.rkt"
  "frame/categorical-series.rkt"
  "stats/tabulate.rkt"
- "plot/plot.rkt"
+ ;"plot/plot.rkt"
  "stats/tabulate.rkt"
  "stats/statistics.rkt"
  (only-in "load/tab-delimited.rkt"
