c2e37424b6e3656ba88ed2d929cd440baa3264ac
Ray Racine <ray.racine@gmail.com>
2013-01-03 18:05:10 -0500

    Local file path as a file: Uri.

--- httpclient/uri/filescheme.rkt
+++ httpclient/uri/filescheme.rkt
@@ -3,11 +3,13 @@
 #lang typed/racket/base
 
 (provide:
+ [local-path->uri (Path -> Uri)]
  [local-file-uri? (Uri -> Boolean)]
  [local-file-uri->path (Uri -> Path)])
 
 (require 
  (only-in "../uri.rkt"
+          parse-uri
           Uri Uri-scheme Uri-path Uri-authority Authority-host))
 
 (: local-file-uri? (Uri -> Boolean))
@@ -18,4 +20,9 @@
 (: local-file-uri->path (Uri -> Path))
 (define (local-file-uri->path uri)  
   (string->path (Uri-path uri)))
+
+; no host name
+(: local-path->uri (Path -> Uri))
+(define (local-path->uri path)
+  (assert (parse-uri (string-append "file://" (path->string path)))))
 
