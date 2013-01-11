72c045d5088191323a8bc8c52a14ae6d03351503
Ray Racine <ray.racine@gmail.com>
2013-01-11 14:43:25 -0500

    Attempt to dynamically load configuration which is a standard Racket module.

--- mapred/config.rkt
+++ mapred/config.rkt
@@ -1,8 +1,14 @@
 #lang typed/racket/base
 
 (provide
+ job-configuration
  DEFAULT-BLOCK-SIZE
+ working-directory
  rdd-materialization-directory)
+
+(require 
+ (only-in "configuration.rkt"
+          Configuration JobConfig InstallConfig InternalsConfig))
 
 ;; The maximum length in bytes a of a data record.
 (define MAX-RECORD-LENGTH 2048)
@@ -12,3 +18,10 @@
 ;; Location of temporary working directory.
 (define rdd-materialization-directory (string->path "/tmp/rktrdd"))
 
+(define working-directory (string->path "/run/shm/ol"))
+
+(: job-configuration (-> Configuration))
+(define (job-configuration)
+  (Configuration (JobConfig "nil" "Null Job" "odbd")
+                 (InstallConfig (string->path "/tmp"))
+                 (InternalsConfig DEFAULT-BLOCK-SIZE)))
