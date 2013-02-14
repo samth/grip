6f51901cfa9b941fec32c773564e84f52b3a9e73
Ray Racine <ray.racine@gmail.com>
2013-02-14 14:50:21 -0500

    Refactored format parsers into dedicated sub-dirs.  - Added simple parser for tabbed files.  - Started work on CSV.

--- RpR/format/fixed/parser.rkt
+++ RpR/format/fixed/parser.rkt
@@ -1,3 +1,21 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ray Racine's Typed Racket Library
+;; Copyright (C) 2007-2013  Raymond Paul Racine
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 #lang typed/racket/base
 
 (provide define-static-parser-for-layout)
@@ -12,7 +30,7 @@
   syntax/parse
   (only-in racket/syntax
            format-id)
-  (only-in "layout-types.rkt"
+  (only-in "../layout-types.rkt"
            Layout-name Layout-fields
            Field-name Field-type
            Field-offset Field-length)))
