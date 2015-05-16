7ac27a8d6ab85cafd9dc2e9eec72f384bb26f3c0
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-05-15 21:28:15 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang setup/infotab
 (define deps '("base" "typed-racket-lib" "r6rs-lib" "srfi-lite-lib"))
-(define build-deps '("scribble-lib"))
+(define build-deps '("racket-doc"
+                     "typed-racket-doc"
+                     "scribble-lib"))
 (define collection 'multi)
